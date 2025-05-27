04563d4e21c994e2de19331095ff5752bb5bdb8e
essmann <66396349+essmann@users.noreply.github.com>
2025-05-27 12:02:54 +0200

    Added new google button and removed the others.

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,28 +1,30 @@
 import "../css/LoginPage.css";
-import { GoogleLogin } from "@react-oauth/google";
+
 import { useNavigate } from "react-router";
 import Header from "../components/Header";
 import { useGoogleLogin } from "@react-oauth/google";
 import getUserInfo from "../api/google/getUserInfo";
+
+import GoogleButton from "react-google-button";
 function LoginPage() {
   const navigate = useNavigate();
-  const loginAuthCode = useGoogleLogin({
+ /*  const loginAuthCode = useGoogleLogin({
     onSuccess: (codeResponse) => {
       console.log(`codeResponse: ${codeResponse}`);
-      
     },
     flow: "auth-code",
-  });
+  }); */
 
   const loginImplicit = useGoogleLogin({
-  onSuccess: tokenResponse => {
-    console.log(tokenResponse);
-    const accessToken = tokenResponse.access_token;
-      
-      getUserInfo(accessToken);
-  }
-});
+    onSuccess: (tokenResponse) => {
+      console.log(tokenResponse);
+      const accessToken = tokenResponse.access_token;
 
+      getUserInfo(accessToken);
+      console.log("Logged in.");
+      navigate("/");
+    },
+  });
 
   return (
     <>
@@ -31,19 +33,11 @@ function LoginPage() {
         <div id="loginPageContainerloginContainer" className="">
           <h2 className="">Sign in to MovieDB</h2>
           <div id="googleLoginContainer">
-            <GoogleLogin
-              onSuccess={(credentialResponse) => {
-                console.log(credentialResponse);
-                navigate("/");
-              }}
-              onError={() => {
-                console.log("Login failed");
-              }}
-            />
-          </div>
-          <button className="bg-red-300 w-lg" onClick={() => loginAuthCode()}>Authoriation Code</button>
-                    <button className="bg-red-300 w-lg" onClick={() => loginImplicit()}>Implicit</button>
 
+          <GoogleButton onClick={() => loginImplicit()}>
+            
+          </GoogleButton>
+          </div>
         </div>
       </div>
     </>
