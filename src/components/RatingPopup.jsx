deff4fed4555578d65cd4f41d04df37589923e78
Essmann <kenwilliam1000@gmail.com>
2025-06-03 19:41:45 +0200

    Fixed ratingpopup

--- src/components/RatingPopup.jsx
+++ src/components/RatingPopup.jsx
@@ -2,10 +2,12 @@ import star_fill from '../assets/star_fill.svg'
 import star_no_fill from '../assets/star_no_fill.svg'
 import handleClickRatingPopup from '../event_handlers/handleClickRatingPopup'
 import { useEffect, useRef } from 'react'
-
-function RatingPopup({ props }) {
+import "../css/MoviePage.css";
+import { useState } from 'react';
+function RatingPopup({ movie, rating, setRating }) {
     const ratingContainer = useRef(null)
     const starContainer = useRef(null)
+    const [selectedRating, setSelectedRating] = useState(null);
     useEffect(() => {
         if (!ratingContainer.hasListeners) {
             console.log(starContainer.current.children)
@@ -29,6 +31,15 @@ function RatingPopup({ props }) {
         
         
     }
+    function handleStarClick(index){
+        setSelectedRating(index);
+        console.log(`Star ${index} clicked!`);
+    }
+    function handleStarContainerMouseOut(){
+        let indexToSet = selectedRating || rating || 0; 
+        console.log(`Mouse left the star container, setting stars to index: ${indexToSet}`);
+        fillStars(indexToSet, false);
+    }
    function fillStars(index, clear) {
        const stars = starContainer.current.children;
        
@@ -49,35 +60,60 @@ function RatingPopup({ props }) {
         }
     });
 }
+function handleRateButtonClick(){
+    let selectedIndex = selectedRating || 0;
+    
+    if(selectedIndex && selectedIndex >0 && selectedIndex <= 10 && selectedIndex !== rating){
+        console.log(`Rating submitted: ${selectedIndex}`);
+        setRating(selectedIndex);
 
+        ratingContainer.current.style.visibility = 'hidden';
+
+        return;
+    }
+    else{
+        console.log(`Invalid rating: ${selectedIndex}`);
+        return;
+}
+}
+function handleRemoveRatingClick(){
+    console.log("Remove rating clicked!");
+    setSelectedRating(null);
+    setRating(null);
+    fillStars(0, true);
+    ratingContainer.current.style.visibility = 'hidden';
+}
+    console.log("rating: ", rating);
+    console.log("selectedRating: ", selectedRating);
     return (
         <div id='ratingPopupContainer' ref={ratingContainer}>
-            <div>{props?.Title || 'Title'}</div>
+            <div id='ratingPopupTitle'>{movie?.title || 'Title'}</div>
             <div
                 id='starRatingContainer'
                 ref={starContainer}
                 onMouseLeave={() =>{
                         console.log('Hovered outside of the stars container!')
-                        fillStars(0, true);
+                        handleStarContainerMouseOut();
                 }
                     
                     
                 }
                 onMouseOver={handleMouseOver}
             >
-                <img src={star_no_fill} alt="" data-index={1} onMouseEnter={handleStarMouseOver} />
-                <img src={star_no_fill} alt="" data-index={2} onMouseEnter={handleStarMouseOver} />
-                <img src={star_no_fill} alt="" data-index={3} onMouseEnter={handleStarMouseOver} />
-                <img src={star_no_fill} alt="" data-index={4} onMouseEnter={handleStarMouseOver} />
-                <img src={star_no_fill} alt="" data-index={5} onMouseEnter={handleStarMouseOver} />
-                <img src={star_no_fill} alt="" data-index={6} onMouseEnter={handleStarMouseOver} />
-                <img src={star_no_fill} alt="" data-index={7} onMouseEnter={handleStarMouseOver} />
-                <img src={star_no_fill} alt="" data-index={8} onMouseEnter={handleStarMouseOver} />
-                <img src={star_no_fill} alt="" data-index={9} onMouseEnter={handleStarMouseOver} />
-                <img src={star_no_fill} alt="" data-index={10} onMouseEnter={handleStarMouseOver} />
+                <img src={star_no_fill} alt="" data-index={1} onMouseEnter={handleStarMouseOver} onClick={e => handleStarClick(e.currentTarget.dataset.index)} />
+                <img src={star_no_fill} alt="" data-index={2} onMouseEnter={handleStarMouseOver} onClick={e => handleStarClick(e.currentTarget.dataset.index)} />
+                <img src={star_no_fill} alt="" data-index={3} onMouseEnter={handleStarMouseOver} onClick={e => handleStarClick(e.currentTarget.dataset.index)} />
+                <img src={star_no_fill} alt="" data-index={4} onMouseEnter={handleStarMouseOver} onClick={e => handleStarClick(e.currentTarget.dataset.index)} />
+                <img src={star_no_fill} alt="" data-index={5} onMouseEnter={handleStarMouseOver} onClick={e => handleStarClick(e.currentTarget.dataset.index)} />
+                <img src={star_no_fill} alt="" data-index={6} onMouseEnter={handleStarMouseOver} onClick={e => handleStarClick(e.currentTarget.dataset.index)} />
+                <img src={star_no_fill} alt="" data-index={7} onMouseEnter={handleStarMouseOver} onClick={e => handleStarClick(e.currentTarget.dataset.index)} />
+                <img src={star_no_fill} alt="" data-index={8} onMouseEnter={handleStarMouseOver} onClick={e => handleStarClick(e.currentTarget.dataset.index)} />
+                <img src={star_no_fill} alt="" data-index={9} onMouseEnter={handleStarMouseOver} onClick={e => handleStarClick(e.currentTarget.dataset.index)} />
+                <img src={star_no_fill} alt="" data-index={10} onMouseEnter={handleStarMouseOver} onClick={e => handleStarClick(e.currentTarget.dataset.index)} />
+
                             </div>
-            <button>Rate</button>
-            <button>Remove rating</button>
+            <button onClick={handleRateButtonClick} id='ratePopupRateButton'>Rate</button>
+            <button onClick={handleRemoveRatingClick} id='ratePopupRemoveButton'>Remove rating</button>
         </div>
     )
 }
